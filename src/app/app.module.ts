dc6537911bf4f544fd7a1ae7e710184654928aa1
Khasanboy Akbarov <Khasanboy.akbarov@thronegaming.com>
2018-07-09 16:24:21 +0300

    finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgRedux, NgReduxModule} from '@angular-redux/store';
+import {FormsModule} from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { rootReducer} from './store/store';
+import { IAppState, INITIAL_STATE } from './store/state';
+import { TodoOverviewComponent } from './components/todo-overview/todo-overview.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoOverviewComponent,
+    TodoListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgReduxModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(ngRedux: NgRedux<IAppState>) {
+    ngRedux.configureStore(rootReducer, INITIAL_STATE);
+  }
+ }
